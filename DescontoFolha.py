2256866bbd48e0b770d340d1c876ee6616035e86
Eduvale007 <eduvale007@gmail.com>
2024-09-30 18:28:32 -0300

    conexao banco de dados

--- DescontoFolha.py
+++ DescontoFolha.py
@@ -1,4 +1,25 @@
-print("\nCálculo de Descontos")
+import mysql.connector
+
+try:
+    conexao = mysql.connector.connect(
+        host="127.0.0.1",     # Porta de conexão
+        user="root",          # Usuário MySQL
+        password="Bololo@10", #Senha do MySQL
+        database="aposentadoriadb", # Nome do banco
+        port=3306             # Porta padrão do MySQL
+    )
+    if conexao.is_connected():
+        print("Conexão estabelecida com sucesso!")
+except mysql.connector.Error as erro:
+    print(f"Erro ao conectar: {erro}")
+finally:
+    if (conexao.is_connected()):
+        conexao.close()
+        print("Conexão encerrada")
+
+
+
+'''print("\nCálculo de Descontos")
 nome = str(input("Digite seu nome completo: "))
 cpf = int(input("Digite seu CPF\n(sem espaços e pontos entre os números): "))
 dataN = str(input("Digite sua data de nascimento\n(no formato: dd/mm/aaaa): "))
@@ -12,3 +33,4 @@ print("do CPF", cpf)
 print("nascido em", dataN)
 print("contratado em", dataC)
 print("tem seu salário com descontos de R${:.2f}".format(novoSalario))
+'''
